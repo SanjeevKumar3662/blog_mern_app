602434292b3bc5856d62ae035e03e8d20c7117f3
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-08 00:11:01 +0530

    feat: add Blog model
    blog model with title , content , description, and uesr id

---
+++ backend/src/models/blog.models.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxlength: 150,
+    },
+    description: {
+      type: String,
+      maxlength: 300,
+    },
+    content: {
+      type: String,
+      required: true,
+      maxlength: 10000,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Blog = mongoose.model("Blog", blogSchema);
