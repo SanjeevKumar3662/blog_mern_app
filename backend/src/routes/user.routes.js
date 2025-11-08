1b2435c89c3af939054517de12bc5eb39f9595f3
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-09 01:07:05 +0530

    feat: add /login route
    - it's a route for user login

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginUser, registerUser } from "../controllers/user.controllers.js";
 
 export const router = Router();
 
-router.route("/test").get((req, res) => {
-  res.status(200).json("your router is working");
-});
+//router.route("/test").get((req, res) => {
+// res.status(200).json("your router is working");
+//});
 
 router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+
 export default router;
