6c20e7259f602dd3d1b48c8992b5a4896f2bfc88
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-09 01:48:45 +0530

    feat: add auth.middleware for accessToken authentication and a /test route
    - includes auth middleware
    - also added a test route for testing auth

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../controllers/user.controllers.js";
+import { authenticateToken } from "../middleware/auth.middlewares.js";
 
 export const router = Router();
 
-//router.route("/test").get((req, res) => {
-// res.status(200).json("your router is working");
-//});
+router.route("/test").get(authenticateToken, (_, res) => {
+  console.log(_.user);
+  return res.status(200).json("your router and auth is working");
+});
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
