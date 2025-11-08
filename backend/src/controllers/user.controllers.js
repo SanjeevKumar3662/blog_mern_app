dddb958ff354adb21cbd1a7f60ebc7f68c77c13b
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-08 23:35:43 +0530

    feat: add fucntions for creating accessToken and refreshToken

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,4 +1,17 @@
 import { User } from "../models/user.models.js";
+import jwt from "jsonwebtoken";
+
+export const generateAccessToken = async (user) => {
+  return jwt.sign(user, process.env.ACCESS_KEY_SECRET, {
+    expiresIn: process.env.ACCESS_KEY_EXPIRY,
+  });
+};
+
+export const generateRefreshToken = async (user) => {
+  return jwt.sign(user, process.env.REFRESH_KEY_SECRET, {
+    expiresIn: process.env.REFRESH_KEY_EXPIRY,
+  });
+};
 
 export const registerUser = async (req, res) => {
   try {
@@ -27,4 +40,25 @@ export const registerUser = async (req, res) => {
       .status(500)
       .json({ message: "failed to register user", error: err.message });
   }
+};
+
+export const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!(username && password)) {
+    return res
+      .status(400)
+      .json({ error: "Either/both username or password not received" });
+  }
+
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.status(400).json({ error: "User not found/exist" });
+  }
+
+  //set user cookies
+  return res
+    .status(200)
+    .json({ message: `${user.username} has logged in successfully` });
 };
