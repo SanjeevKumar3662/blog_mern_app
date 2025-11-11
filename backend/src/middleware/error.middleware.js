7fc5509f0f194fb08ec99e78587f734fa6b18476
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-11 15:11:26 +0530

    fix : corrected import path form ApiError

--- backend/src/middleware/error.middleware.js
+++ backend/src/middleware/error.middleware.js
@@ -1,4 +1,4 @@
-import { ApiError } from "../utils/ApiError.js";
+import { ApiError } from "../utils/apiError.js";
 
 export const errorMiddleware = (err, req, res, next) => {
   // Log the error details (only in development)
