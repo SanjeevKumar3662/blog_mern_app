47fe5a7e3dc832aca29447916e6f2cba0ca1668f
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-11 12:23:56 +0530

    feat: add asyncHandler
    - this takes async function as argument and wraps then in try catch

---
+++ backend/src/middleware/asyncHandler.js
@@ -0,0 +1,10 @@
+export const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    return res.status(error.status || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controllers.js";
+import {
+  loginUser,
+  refreshExpiredAccessToken,
+  registerUser,
+} from "../controllers/user.controllers.js";
 import { authenticateToken } from "../middleware/auth.middlewares.js";
-
+import { asyncHandler } from "../middleware/asyncHandler.js";
 export const router = Router();
 
 router.route("/test").get(authenticateToken, (_, res) => {
@@ -9,7 +13,8 @@ router.route("/test").get(authenticateToken, (_, res) => {
   return res.status(200).json("your router and auth is working");
 });
 
-router.route("/register").post(registerUser);
-router.route("/login").post(loginUser);
+router.post("/register", asyncHandler(registerUser));
+router.post("/login", asyncHandler(loginUser));
+router.post("/refreshToken", asyncHandler(refreshExpiredAccessToken));
 
 export default router;
