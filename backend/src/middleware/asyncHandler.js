96c599b29a8a9a5bed962666a99880fd1bbfbff5
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-12 18:36:38 +0530

    feat: add error loggin for asyncHandler
    - in dev it will log the whole error obj
    - in prod it will only log error.message

--- backend/src/middleware/asyncHandler.js
+++ backend/src/middleware/asyncHandler.js
@@ -2,6 +2,11 @@ export const asyncHandler = (fn) => async (req, res, next) => {
   try {
     await fn(req, res, next);
   } catch (error) {
+    if (process.env.MODE === "DEV") {
+      console.error(error);
+    } else {
+      console.error(error.message);
+    }
     return res.status(error.status || 500).json({
       success: false,
       message: error.message,
